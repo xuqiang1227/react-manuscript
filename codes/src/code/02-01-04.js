49cb9ca5b1ef7e0bc2b4119134db7d3c4d1a690b
kairi1227 <kairi1227@126.com>
2018-08-27 17:08:08 +0800

    add counter code

--- codes/src/code/02-01-04.js
+++ codes/src/code/02-01-04.js
@@ -1,1 +1,38 @@
-export default ``;
+export default `
+import React from 'react';
+import { addActioner, subActioner, resetActioner } from '../../actions/CounterAction';
+
+export default ({ counter = 0, dispatch }) => {
+  const styles = {
+    display: 'flex',
+    justifyContent: 'space-around'
+  };
+
+  const updateCounter = (type) => {
+    switch (type) {
+      case 'add':
+        dispatch(addActioner({ counter }));
+        break;
+      case 'sub':
+        dispatch(subActioner({ counter }));
+        break;
+      case 'reset':
+        dispatch(resetActioner());
+        break;
+      default:
+        dispatch(resetActioner());
+    }
+  };
+
+  return (
+    <div>
+      <h1>当前计数为：{counter}</h1>
+      <div style={styles}>
+        <button onClick={() => updateCounter('add')}>加</button>
+        <button onClick={() => updateCounter('sub')}>减</button>
+        <button onClick={() => updateCounter('reset')}>重置</button>
+      </div>
+    </div>
+  );
+};
+`;
